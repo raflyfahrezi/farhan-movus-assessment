e69ae6a59716579c957d3ffa13ba63a9c14e6fc1
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-10 23:49:45 +0700

    fix app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { Dashboard } from '@/modules'
 import { PageWrapper } from '@/layout'
 
 const App = () => {
-    return <PageWrapper>{/*  */}</PageWrapper>
+    return (
+        <PageWrapper>
+            <Dashboard />
+        </PageWrapper>
+    )
 }
 
 export default App
