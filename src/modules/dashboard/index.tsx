0955190b1213e975a3df5fc4f82cd09928804c72
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-12 14:19:12 +0700

    fix dashboard

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,32 +1,20 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import { Wrapper } from '@/components'
-import { TVehicleMake } from '@/models'
 
 import { sDashboard } from './styles'
-import { fetchVehicleMakeData } from './helpers'
 
 import Counter from './counter'
+import MakerChart from './makerChart'
 
 const Dashboard = () => {
-    const [vehicleMakeList, setVehicleMakeList] = useState<TVehicleMake[]>([])
-
-    const fetchData = async () => {
-        const vehicleMakeDataResponse = await fetchVehicleMakeData()
-
-        setVehicleMakeList(vehicleMakeDataResponse)
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-
     return (
         <Wrapper>
             <div className={sDashboard}>
-                <Counter vehicleMakesTotal={vehicleMakeList.length} />
+                <Counter />
+                <MakerChart />
             </div>
         </Wrapper>
     )
