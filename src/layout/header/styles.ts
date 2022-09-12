2a01ee54f6012378071cd39b6e7ec17733e575b2
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-12 18:08:35 +0700

    fix header

--- src/layout/header/styles.ts
+++ src/layout/header/styles.ts
@@ -3,9 +3,7 @@ import { css } from '@emotion/css'
 export const sHeader = css`
     width: 100%;
 
-    text-align: left;
+    text-align: center;
 
     padding: 20px;
-
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
 `
