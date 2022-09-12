ba816a990f2b59a5ea76d8ec63c50fed2b4dc562
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-12 18:02:41 +0700

    fix bar chart

--- src/components/chart/bar/index.tsx
+++ src/components/chart/bar/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import { Chart as ChartJS } from 'chart.js'
 import { Bar } from 'react-chartjs-2'
+import React, { useRef, useEffect } from 'react'
 
 import { sBarChart } from './styles'
 import { TBarChartProps } from './types'
 
 const BarChart = ({ label, labels, datasets }: TBarChartProps) => {
+    const barChartRef = useRef<ChartJS<'bar', number[], string> | null>()
+
+    useEffect(() => {
+        barChartRef.current?.update()
+    }, [label, labels, datasets])
+
     return (
         <Bar
+            ref={barChartRef}
             className={sBarChart}
             data={{
                 labels: labels,
