ff705ba4334512012cb566afc1523aad08e3cc38
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2022-09-12 14:18:26 +0700

    fix app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js'
 
 import { Dashboard } from '@/modules'
 import { PageWrapper } from '@/layout'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 const App = () => {
     return (
